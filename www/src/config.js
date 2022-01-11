74e0e0108cd5f6e373d9290f04731fa3e252793a
graystum <graystum.dev@gmail.com>
2022-01-11 23:39:35 +0100

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "16v7omt4uinuq5bq35nr6l5en9",     // CognitoClientID
+  "api_base_url": "https://f597yzuaw4.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-graystumstack.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d3rbisvjiu9qfz.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
